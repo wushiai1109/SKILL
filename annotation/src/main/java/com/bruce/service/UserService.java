e2ee7944de9f6b3735e8bab69c636fbec17645a9
wushiai <2625957707@qq.com>
2020-11-22 09:41:53 +0800

    change log4j2 code to @Slf4j

--- annotation/src/main/java/com/bruce/service/UserService.java
+++ annotation/src/main/java/com/bruce/service/UserService.java
@@ -3,6 +3,7 @@ package com.bruce.service;
 import com.bruce.mapper.UserMapper;
 import com.bruce.model.DO.UserDO;
 import com.bruce.model.VO.ResultInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Service
 @Transactional(rollbackFor = RuntimeException.class)
+@Slf4j
 public class UserService {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+//    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserMapper userMapper;
@@ -34,7 +36,8 @@ public class UserService {
         if (u != null) {
             request.getSession().setAttribute("user", u);
             info.setFlag(true);
-            logger.info("login —— user:"+user.toString());
+//            logger.info("login —— user:"+user.toString());
+            log.info("login —— user:" + user.toString());
             info.setMsg("登陆成功");
             info.setData(u);
         } else {
